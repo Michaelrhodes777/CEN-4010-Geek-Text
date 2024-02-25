51d61e1ccc2ff0581ca13037a2112e2cf60e7d2e
David Reyes <davidusa221113@gmail.com>
2024-02-25 16:51:14 -0500

    authorization-updated

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,17 +1,33 @@
+const jwt = require('jsonwebtoken');
+const { InvalidAccessTokenError, ExpiredAccessTokenError } = require('./util/custom-errors');
 
-const { InvalidAccessTokenError } = require('./util/custom-errors');
-
-const verifyRoles = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!req.roles) throw new InvalidAccessTokenError('No roles provided.');
-
-    const rolesArray = [...allowedRoles];
-    const result = req.roles.map(role => rolesArray.includes(role)).find(val => val === true);
-    
-    if (!result) throw new InvalidAccessTokenError('User does not have the required roles.');
-
-    next();
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader?.startsWith('Bearer ')) {
+    // Here we throw a new InvalidAccessTokenError instead of returning a 401 status directly
+    return next(new InvalidAccessTokenError('No token provided.'));
   }
-}
+  const token = authHeader.split(' ')[1];
+  console.log(token)
+  
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        // Differentiate between expired token and other token related errors
+        if (err.name === 'TokenExpiredError') {
+          return next(new ExpiredAccessTokenError('Token has expired.'));
+        } else {
+          return next(new InvalidAccessTokenError('Token is invalid.'));
+        }
+      }
+      
+      req.user = decoded.UserInfo.username;
+      req.roles = decoded.UserInfo.roles;
+      next();
+    }
+  );
+};
 
-module.exports = verifyRoles;
+module.exports = verifyJWT;
