b7848a653c935c2244af76119dc3a68e24523851
liz-per <157546841+liz-per@users.noreply.github.com>
2024-03-28 16:57:38 -0400

    Added shoppingCartsLTE2EBase (#43)

---
+++ backend/testing_utils/linking_tables/shopping_carts_lt/shoppingCartsLTE2EBase.js
@@ -0,0 +1,74 @@
+const shoppingCartsLTE2EBase = {
+    identifier: "shopping_carts_lt",
+    data: [
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        },
+        {
+            "user_id_fkey": undefined,
+            "book_id_fkey": undefined,
+            "quantity": 1
+        }
+    ],
+    fkeyReferences: [
+        {
+            identifier: "users",
+            internalIndexes: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ],
+            externalIndexes: [ 0, 1, 2, 3, 4, 5, 0, 1, 2, 3,  4,  0,  1, 2 ]
+        },
+        {
+            identifier: "books",
+            internalIndexes: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ],
+            externalIndexes: [ 0, 1, 2, 3, 4, 5, 0, 1, 2, 3,  4,  0,  1, 2 ]
+        }
+    ]
+};
+
+module.exports = shoppingCartsLTE2EBase;
