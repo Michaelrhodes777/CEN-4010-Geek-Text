6d18ee5e718e7178febb5cd78083e26a077a4836
Michaelrhodes777 <110749095+Michaelrhodes777@users.noreply.github.com>
2024-03-29 19:31:35 -0400

    Add isbn (#46)
    * added isbn functionality
    
    * e2e for isbn

---
+++ backend/routes/read_only_views/books_by_isbn/isbnE2E.test.js
@@ -0,0 +1,34 @@
+const supertest = require('supertest');
+const DatabaseControl = require('../../../testing_utils/DatabaseControl.js');
+const { clientFactory } = require('../../../database/setupFxns.js');
+const createServer = require('../../../util/createServer.js');
+const TablesConsumables = require('../../../testing_utils/tables/TablesConsumables.js');
+const { tableNamesMap, idMap, tablesE2EBaseMap } = TablesConsumables;
+
+const identifiers = [ tableNamesMap.authors, tableNamesMap.publishers, tableNamesMap.genres, tableNamesMap.books ];
+
+const databaseInstantiationPayload = {
+    identifiers,
+    nonCascadeDeletions: identifiers,
+    dataPayloads: identifiers.map((identifier) => tablesE2EBaseMap[identifier])
+};
+
+const databaseControl = new DatabaseControl(databaseInstantiationPayload);
+beforeAll(async () => {
+    await databaseControl.setupDatabase();
+});
+afterAll(async () => {
+    await databaseControl.tearDownDatabase();
+});
+
+// http://localhost:3500/books_by_isbn/:isbn
+// http://localhost:3500/books_by_isbn/712984719283479182
+
+describe("GET books_by_isn: Validate correct database instantiation and GET functionality", () => {
+    test(`\n\tGET request`, async () => {
+        const { isbn } = databaseControl.dataPackages.books.rows[0];
+        const res = await supertest(createServer())
+            .get(`/books_by_isbn/${isbn}`)
+            .expect(200);
+    });
+});
